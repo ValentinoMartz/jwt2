52f5c704d44511434f81a452d05fd6021842eb8f
ValentinoMartz <valentinomartinez55@gmail.com>
2022-09-12 17:56:15 -0300

    all in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dashboard";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route element={<Register />} path="/" />
+          <Route element={<Login />} path="/login" />
+          <Route element={<Dashboard />} path="/dashboard" />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Register.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const req = await fetch("http://localhost:1337/api/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: await JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    const data = await req.json();
+
+    if (data.status === "ok") {
+      navigate("/login");
+    } else {
+      alert("duplicate Email");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={handleRegister}>
+        <input
+          placeholder="Name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <input
+          placeholder="Email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          placeholder="Password"
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <input type="submit" />
+        <br />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
